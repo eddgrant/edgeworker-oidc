befe03f8f76f4c9df3fa2951b53d57a63f9de0c8
eddgrant <edd@eddgrant.com>
2025-05-09 13:20:20 +0100

    Arrange tests in functionally similar groups

--- centralized/edgeworker/test/main.test.ts
+++ centralized/edgeworker/test/main.test.ts
@@ -28,129 +28,135 @@ describe('OIDC Response Provider', () => {
         vi.clearAllMocks();
     });
 
-    it("should return 404 for unknown path", async () => {
-        // Given
-        vi.mock('create-response')
-        vi.mock('response')
+    describe("404 tests", () => {
+        it("should return 404 for unknown path", async () => {
+            // Given
+            vi.mock('create-response')
+            vi.mock('response')
 
-        createResponse.mockImplementation(function () {
-            return new ResponseBuilder().withStatus(notFoundStatusCode).withHeaders(responseHeaders).build()
+            createResponse.mockImplementation(function () {
+                return new ResponseBuilder().withStatus(notFoundStatusCode).withHeaders(responseHeaders).build()
+            })
+            const request = new ResponseProviderRequestBuilder().withDefaults().build()
+            const requestMock = vi.mocked(request, true)
+
+            // When
+            const returnedResponse : any = await responseProvider(requestMock);
+
+            // Then
+            expect(createResponse).toHaveBeenCalledWith(404, {'Content-Type': ['application/text']},`No route for ${request.url}`)
+            expect(returnedResponse.status).toBe(notFoundStatusCode)
+            expect(returnedResponse.getHeaders()).toStrictEqual(responseHeaders)
+        });
+    });
+
+    describe("/login tests", () => {
+        it("A request to /login should return an OIDC login response", async () => {
+            // Given
+            vi.mock('create-response')
+            vi.mock('response')
+            vi.mock('cookies')
+
+            const loginPath = `${basePath}login`;
+            const request = new ResponseProviderRequestBuilder()
+                .withHost(host)
+                .withPath(loginPath)
+                .withQueryParam("url", oidcUrl)
+                .withVariable("PMUSER_MANDS_AKSECRET", akamaiSecret)
+                .withVariable("PMUSER_MANDS_CLIENTID", clientId)
+                .withVariable("PMUSER_MANDS_SECRET", secret)
+                .withVariable("PMUSER_MANDS_AUTH_URL", authUrl)
+                .build()
+            const requestMock = vi.mocked(request, true)
+
+            // When
+            const returnedResponse : any = await responseProvider(requestMock);
+            //TODO: Would it be valuable to make assertions on the returnedResponse?
+
+            // Then
+            const createResponseCalls = createResponse.mock.calls
+            expect(createResponseCalls.length).toBe(1);
+
+            const createResponseCallArguments = createResponseCalls[0]
+            const statusCode = createResponseCallArguments[0]
+            expect(statusCode).toBe(302)
+
+            const responseHeaders = createResponseCallArguments[1]
+            const setCookieResponseHeaders = responseHeaders['set-cookie']
+            expect(setCookieResponseHeaders.length).toBe(2)
+
+            const oidcCookieResponseHeader = setCookieResponseHeaders[0]
+            // This is a rather crude way of asserting that the expected parameters were passed to the SetCookie constructor.
+            expect(oidcCookieResponseHeader).toBe(`__Secure-oidcurl="${oidcUrl}"; path=${basePath}; Secure; HttpOnly`)
+
+            const nonceResponseHeader = setCookieResponseHeaders[1]
+            // This is a rather crude way of asserting that the expected parameters were passed to the SetCookie constructor.
+            expect(nonceResponseHeader).toMatch(new RegExp(`^__Secure-nonce="[a-z0-9]{8}"; path=${basePath}; Secure; HttpOnly$`))
+
+            const locationResponseHeader = new URL(responseHeaders['location'][0])
+            expect(locationResponseHeader.protocol).toMatch(new RegExp(`^${authScheme}?`))
+            expect(locationResponseHeader.host).toMatch(authHost)
+            expect(locationResponseHeader.pathname).toMatch(authPath)
+            expect(locationResponseHeader.searchParams.get("client_id")).toMatch(clientId)
+            expect(locationResponseHeader.searchParams.get("nonce").length).toBe(8)
+            expect(locationResponseHeader.searchParams.get("redirect_uri")).toMatch(`https://${host}${basePath}callback`)
+            expect(locationResponseHeader.searchParams.get("response_type")).toMatch("code")
+            expect(locationResponseHeader.searchParams.get("scope")).toMatch("openid email")
+
+            const providedBody = createResponseCallArguments[2]
+            expect(providedBody).toEqual("")
         })
-        const request = new ResponseProviderRequestBuilder().withDefaults().build()
-        const requestMock = vi.mocked(request, true)
-
-        // When
-        const returnedResponse : any = await responseProvider(requestMock);
-
-        // Then
-        expect(createResponse).toHaveBeenCalledWith(404, {'Content-Type': ['application/text']},`No route for ${request.url}`)
-        expect(returnedResponse.status).toBe(notFoundStatusCode)
-        expect(returnedResponse.getHeaders()).toStrictEqual(responseHeaders)
     });
 
-    it("A request to /login should return an OIDC login response", async () => {
-        // Given
-        vi.mock('create-response')
-        vi.mock('response')
-        vi.mock('cookies')
+    describe("/callback tests", () => {
+        it("A request to /callback returns a 400 response when no 'code' query parameter is provided", async () => {
+            // Given
+            vi.mock('create-response')
 
-        const loginPath = `${basePath}login`;
-        const request = new ResponseProviderRequestBuilder()
-            .withHost(host)
-            .withPath(loginPath)
-            .withQueryParam("url", oidcUrl)
-            .withVariable("PMUSER_MANDS_AKSECRET", akamaiSecret)
-            .withVariable("PMUSER_MANDS_CLIENTID", clientId)
-            .withVariable("PMUSER_MANDS_SECRET", secret)
-            .withVariable("PMUSER_MANDS_AUTH_URL", authUrl)
-            .build()
-        const requestMock = vi.mocked(request, true)
+            const callbackPath = `${basePath}callback`;
+            const request = new ResponseProviderRequestBuilder()
+                .withHost(host)
+                .withPath(callbackPath)
+                .withQueryParam("url", oidcUrl)
+                .build()
+            const requestMock = vi.mocked(request, true)
 
-        // When
-        const returnedResponse : any = await responseProvider(requestMock);
-        //TODO: Would it be valuable to make assertions on the returnedResponse?
+            // When
+            const returnedResponse : any = await responseProvider(requestMock);
 
-        // Then
-        const createResponseCalls = createResponse.mock.calls
-        expect(createResponseCalls.length).toBe(1);
+            // Then
+            const expectedResponseBody = {
+                error: "precondition",
+                description: `callback request not initiated, redirect-url:/, query:${request.query}`
+            }
+            expect(createResponse).toHaveBeenCalledWith(400, {'content-type': ['application/json']}, JSON.stringify(expectedResponseBody))
+        })
 
-        const createResponseCallArguments = createResponseCalls[0]
-        const statusCode = createResponseCallArguments[0]
-        expect(statusCode).toBe(302)
+        it("A request to /callback should return an OIDC callback response", async () => {
+            //TODO: Implement this test.
 
-        const responseHeaders = createResponseCallArguments[1]
-        const setCookieResponseHeaders = responseHeaders['set-cookie']
-        expect(setCookieResponseHeaders.length).toBe(2)
+            // Given
+            vi.mock('create-response')
+            // TODO: Mock cookies.get("oidcurl")
+            // TODO: Mock params.get("code")
+            // TODO: Mock request.getHeader('Cookie')
+            const callbackPath = `${basePath}callback`;
+            const request = new ResponseProviderRequestBuilder()
+                .withHost(host)
+                .withPath(callbackPath)
+                .withQueryParam("url", oidcUrl)
+                .withVariable("PMUSER_MANDS_AKSECRET", akamaiSecret)
+                .withVariable("PMUSER_MANDS_CLIENTID", clientId)
+                .withVariable("PMUSER_MANDS_SECRET", secret)
+                .withVariable("PMUSER_MANDS_AUTH_URL", authUrl)
+                .build()
+            const requestMock = vi.mocked(request, true)
 
-        const oidcCookieResponseHeader = setCookieResponseHeaders[0]
-        // This is a rather crude way of asserting that the expected parameters were passed to the SetCookie constructor.
-        expect(oidcCookieResponseHeader).toBe(`__Secure-oidcurl="${oidcUrl}"; path=${basePath}; Secure; HttpOnly`)
+            // When
+            const returnedResponse : any = await responseProvider(requestMock);
+            //TODO: Would it be valuable to make assertions on the returnedResponse?
 
-        const nonceResponseHeader = setCookieResponseHeaders[1]
-        // This is a rather crude way of asserting that the expected parameters were passed to the SetCookie constructor.
-        expect(nonceResponseHeader).toMatch(new RegExp(`^__Secure-nonce="[a-z0-9]{8}"; path=${basePath}; Secure; HttpOnly$`))
-
-        const locationResponseHeader = new URL(responseHeaders['location'][0])
-        expect(locationResponseHeader.protocol).toMatch(new RegExp(`^${authScheme}?`))
-        expect(locationResponseHeader.host).toMatch(authHost)
-        expect(locationResponseHeader.pathname).toMatch(authPath)
-        expect(locationResponseHeader.searchParams.get("client_id")).toMatch(clientId)
-        expect(locationResponseHeader.searchParams.get("nonce").length).toBe(8)
-        expect(locationResponseHeader.searchParams.get("redirect_uri")).toMatch(`https://${host}${basePath}callback`)
-        expect(locationResponseHeader.searchParams.get("response_type")).toMatch("code")
-        expect(locationResponseHeader.searchParams.get("scope")).toMatch("openid email")
-
-        const providedBody = createResponseCallArguments[2]
-        expect(providedBody).toEqual("")
-    });
-
-    it("A request to /callback returns a 400 response when no 'code' query parameter is provided", async () => {
-        // Given
-        vi.mock('create-response')
-
-        const callbackPath = `${basePath}callback`;
-        const request = new ResponseProviderRequestBuilder()
-            .withHost(host)
-            .withPath(callbackPath)
-            .withQueryParam("url", oidcUrl)
-            .build()
-        const requestMock = vi.mocked(request, true)
-
-        // When
-        const returnedResponse : any = await responseProvider(requestMock);
-
-        // Then
-        const expectedResponseBody = {
-            error: "precondition",
-            description: `callback request not initiated, redirect-url:/, query:${request.query}`
-        }
-        expect(createResponse).toHaveBeenCalledWith(400, {'content-type': ['application/json']}, JSON.stringify(expectedResponseBody))
-    })
-
-    it("A request to /callback should return an OIDC callback response", async () => {
-        //TODO: Implement this test.
-
-        // Given
-        vi.mock('create-response')
-        // TODO: Mock cookies.get("oidcurl")
-        // TODO: Mock params.get("code")
-        // TODO: Mock request.getHeader('Cookie')
-        const callbackPath = `${basePath}callback`;
-        const request = new ResponseProviderRequestBuilder()
-            .withHost(host)
-            .withPath(callbackPath)
-            .withQueryParam("url", oidcUrl)
-            .withVariable("PMUSER_MANDS_AKSECRET", akamaiSecret)
-            .withVariable("PMUSER_MANDS_CLIENTID", clientId)
-            .withVariable("PMUSER_MANDS_SECRET", secret)
-            .withVariable("PMUSER_MANDS_AUTH_URL", authUrl)
-            .build()
-        const requestMock = vi.mocked(request, true)
-
-        // When
-        const returnedResponse : any = await responseProvider(requestMock);
-        //TODO: Would it be valuable to make assertions on the returnedResponse?
-
-        // Then
+            // Then
+        })
     });
 });
